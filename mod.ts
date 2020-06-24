47d0a9f9338a57607e89f381c323ee6f96490b78
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2020-06-24 20:16:13 +0200

    more launch data mapping

--- mod.ts
+++ mod.ts
@@ -1,8 +1,11 @@
 import * as log from 'http://deno.land/std/log/mod.ts'
+import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.15-es/lodash.js"
 
 interface Launch {
   flightNumber: number;
   mission: string;
+  rocket: string;
+  customers: string[];
 }
 const launches = new Map<number, Launch>();
 
@@ -19,9 +22,15 @@ async function downloadLaunchData() {
 
   const launchData = await response.json();
   for (const launch of launchData) {
+    const payloads = launch['rocket']['second_stage']['payloads'];
+    const customers = _.flatMap(payloads, (payload: any) => {
+      return payload['customers'];
+    })
     const flightData = {
       flightNumber: launch['flight_number'],
       mission: launch['mission_name'],
+      rocket: launch['rocket']['rocket_name'],
+      customers,
     };
     launches.set(flightData.flightNumber, flightData)
     log.info(JSON.stringify(flightData))
