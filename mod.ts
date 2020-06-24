68e56315334cedc898967b854df078ee04adf06b
Krzysiek Kobyliński <kkobylinski@bevy.pl>
2020-06-24 20:00:20 +0200

    deno logging

--- mod.ts
+++ mod.ts
@@ -1,10 +1,29 @@
+import * as log from 'http://deno.land/std/log/mod.ts'
+
+log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    default: {
+      level: 'DEBUG',
+      handlers: ['console'],
+    },
+  }
+})
 async function downloadLaunchData() {
+  log.info('downloading launch data...')
   const response = await fetch('https://api.spacexdata.com/v3/launches', {
     method: 'GET',
   });
 
+  if (!response.ok) {
+    log.warning('Problem downloading launch data');
+    throw new Error('launch data download failed');
+  }
+
   const launchData = await response.json();
-  console.log(launchData)
+  // console.log(launchData)
 }
 
 await downloadLaunchData();
